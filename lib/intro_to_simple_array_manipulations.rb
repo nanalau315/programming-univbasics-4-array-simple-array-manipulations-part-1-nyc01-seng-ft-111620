ea38222115da147652a36e43b8e16c69a1890029
Wing Nam Lau <nanalau315@hotmail.com>
2020-10-31 04:02:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push(colors_in_the_rainbow, next_color)
+  updated_array = colors_in_the_rainbow.push(next_color)
+  updated_array
+end
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc.unshift(new_neighborhood)
+  bouroughs_in_nyc
+end
+
+def using_pop (array)
+  array.pop
+end
+
+def pop_with_args(array)
+  new_array = []
+  new_array << array.pop
+  second_doggy = array.pop
+  new_array.unshift(second_doggy)
+  new_array
+end
